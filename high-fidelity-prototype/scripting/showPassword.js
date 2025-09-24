9032afc1f316ee9aa3e6d22943ae50ac9e5bb836
c-pierce-1 <c.pierce27284@gmail.com>
2025-09-24 04:41:11 -0400

    Login/signup pages functionality complete

--- high-fidelity-prototype/scripting/showPassword.js
+++ high-fidelity-prototype/scripting/showPassword.js
@@ -1,8 +1,20 @@
 function myFunction() {
-  var x = document.getElementById("myInput");
+  var x = document.getElementById("inputPassword");
   if (x.type === "password") {
     x.type = "text";
   } else {
     x.type = "password";
   }
 }
+
+function passwordRule() {
+  const passwordInput = document.getElementById("inputPassword");
+  if (passwordInput) {
+    passwordInput.addEventListener("keydown", function (e) {
+      if (e.code === "Space") {
+        e.preventDefault();
+      }
+    });
+  }
+}
+passwordRule();
