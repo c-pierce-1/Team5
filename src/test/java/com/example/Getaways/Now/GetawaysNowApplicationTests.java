2576f7bcaba45f94f6451686eac886c1a457db4f
Sarah Parisi <siparisi@uncg.edu>
2025-10-22 11:14:50 -0400

    Initial Setup

---
+++ src/test/java/com/example/Getaways/Now/GetawaysNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Getaways.Now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetawaysNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
