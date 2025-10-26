d146c7188ca389cd7276ce5ad00bef331810f08d
c-pierce-1 <c.pierce27284@gmail.com>
2025-10-26 19:50:32 -0400

    Added springboot project files

--- backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/GetawaysNowApplication.java
+++ backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/GetawaysNowApplication.java
@@ -1,4 +1,4 @@
-package com.example.Getaways.Now;
+package com.example.GetawaysNow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
