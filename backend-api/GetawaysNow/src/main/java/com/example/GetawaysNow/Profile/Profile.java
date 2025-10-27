c792d7b0008b52c7c12a7aca1e24f95dbaa25ba2
c-pierce-1 <c.pierce27284@gmail.com>
2025-10-27 19:28:08 -0400

    Added all files needed

--- backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/Profile/Profile.java
+++ backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/Profile/Profile.java
@@ -1,13 +1,13 @@
-package com.example.GetawaysNow.Profile;
+package com.example.getawaysnow.profile;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 
 
 @Entity
