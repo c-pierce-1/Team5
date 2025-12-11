c56f7181eeefc780485a3d686b6f9e141d5a2aa1
c-pierce-1 <c.pierce27284@gmail.com>
2025-12-10 20:31:26 -0500

    signup show password logic fixed, checkout page updated.

--- backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/booking/BookingController.java
+++ backend-api/GetawaysNow/src/main/java/com/example/GetawaysNow/booking/BookingController.java
@@ -1,5 +1,7 @@
 package com.example.GetawaysNow.booking;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,15 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.GetawaysNow.listing.Listing;
+import com.example.GetawaysNow.listing.ListingRepository;
+import com.example.GetawaysNow.listingImages.ListingImages;
+import com.example.GetawaysNow.listingImages.ListingImagesRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class BookingController{
     private final BookingService bookingService;
+    private final ListingRepository listingRepository;
+    private final ListingImagesRepository listingImagesRepository;
 
     @Autowired
-    public BookingController(BookingService bookingService){
+    public BookingController(BookingService bookingService, ListingRepository listingRepository, ListingImagesRepository listingImagesRepository){
         this.bookingService = bookingService;
+        this.listingRepository = listingRepository;
+        this.listingImagesRepository = listingImagesRepository;
     }
 
     @GetMapping("/checkout/listing/{listingId}")
@@ -24,6 +37,12 @@ public class BookingController{
         if (profileId == null){
             return "redirect:/login";
         }
+
+        Listing listing = listingRepository.findById(listingId).orElseThrow(() -> new EntityNotFoundException("Listing not found"));
+        List<ListingImages> images = listingImagesRepository.findBylistingID(listing);
+
+        model.addAttribute("images", images);
+        model.addAttribute("listing", listing);
         
         model.addAttribute("booking", new Booking());
         model.addAttribute("profileId", profileId);
